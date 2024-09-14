6ed73b19e00698717c2a8f82bf682cffffc08b15
Swetha Neha <swethaneha.sivakumar@gmail.com>
2024-09-14 16:47:27 -0700

    supervised learning (multiple linear regression, logistic regression, neural networks, and decision trees), unsupervised learning (clustering, dimensionality reduction, recommender systems) and Advanced Learning Algorithms

---
+++ Unsupervise ML/Reinforcement Learning/State-action value function example (1).py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # State Action Value Function Example
+# 
+# In this Jupyter notebook, you can modify the mars rover example to see how the values of Q(s,a) will change depending on the rewards and discount factor changing.
+
+# In[1]:
+
+
+import numpy as np
+from utils import *
+
+
+# In[10]:
+
+
+# Do not modify
+num_states = 6
+num_actions = 2
+
+
+# In[23]:
+
+
+terminal_left_reward = 100
+terminal_right_reward = 70
+each_step_reward = 0
+
+# Discount factor
+gamma = 0.5
+
+# Probability of going in the wrong direction
+misstep_prob = 0
+
+
+# In[24]:
+
+
+generate_visualization(terminal_left_reward, terminal_right_reward, each_step_reward, gamma, misstep_prob)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
